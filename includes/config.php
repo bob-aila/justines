992dfe47c56070a2f3b5884596fef3da3223a8a5
bob-aila <61519243+bob-aila@users.noreply.github.com>
2021-11-21 03:39:28 +0300

    Update config.php

--- includes/config.php
+++ includes/config.php
@@ -2,7 +2,7 @@
 
 //Database Constants
 defined('DB_SERVER') ? null : define("DB_SERVER","MYSQL5037.site4now.net");//define our database server
-defined('DB_USER') ? null : define("DB_USER","a7cc25_reserva");		  //define our database user	
+defined('DB_USER') ? null : define("DB_USER","a7cc25_reserv");		  //define our database user	
 defined('DB_PASS') ? null : define("DB_PASS","mt00268018");			  //define our database Password	
 defined('DB_NAME') ? null : define("DB_NAME","db_a7cc25_reserva"); //define our database Name
 
