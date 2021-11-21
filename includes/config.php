531c9b04de8281e70cfeedc85fb2ac0540579d18
bob-aila <61519243+bob-aila@users.noreply.github.com>
2021-11-21 03:08:37 +0300

    Update config.php

--- includes/config.php
+++ includes/config.php
@@ -1,10 +1,10 @@
 <?php
 
 //Database Constants
-defined('DB_SERVER') ? null : define("DB_SERVER","localhost");//define our database server
-defined('DB_USER') ? null : define("DB_USER","root");		  //define our database user	
-defined('DB_PASS') ? null : define("DB_PASS","");			  //define our database Password	
-defined('DB_NAME') ? null : define("DB_NAME","reservation"); //define our database Name
+defined('DB_SERVER') ? null : define("DB_SERVER","MYSQL5037.site4now.net");//define our database server
+defined('DB_USER') ? null : define("DB_USER","a7cc25_reserva");		  //define our database user	
+defined('DB_PASS') ? null : define("DB_PASS","mt00268018");			  //define our database Password	
+defined('DB_NAME') ? null : define("DB_NAME","db_a7cc25_reserva"); //define our database Name
 
 $thisFile = str_replace('\\', '/', __FILE__);
 $docRoot =$_SERVER['DOCUMENT_ROOT'];
