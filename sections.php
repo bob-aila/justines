d37629a2ed3a6962b86c6047539ea9fd73101693
bob-aila <61519243+bob-aila@users.noreply.github.com>
2021-11-22 12:51:32 +0000

    Add files via upload

---
+++ sections.php
@@ -0,0 +1,31 @@
+
+       <section id="home">
+           <?php
+            include_once 'home.php';
+		   ?>
+        </section>
+        <section id="index.php?page=2">
+		    <?php
+              include_once 'gallery.php';
+            ?> 
+        </section>
+		<section id="index.php?page=3">
+		    <?php
+              include_once 'about.php';
+            ?> 
+        </section>
+		<section id="index.php?page=4">
+		    <?php
+ 		       include_once 'contact.php';		
+            ?> 
+        </section>
+		<section id="index.php?page=5">
+		    <?php
+                include_once 'contact.php';
+            ?>  
+        </section>
+		<section id="index.php?page=7">
+		    <?php
+                include_once 'sitemap.php';
+            ?>           
+        </section>
