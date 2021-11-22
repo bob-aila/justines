815d5cb581f38fef1fce4e3750fcf711526aea7e
bob-aila <61519243+bob-aila@users.noreply.github.com>
2021-11-22 15:46:39 +0300

    Update config.php

--- includes/config.php
+++ includes/config.php
@@ -1,8 +1,11 @@
 <?php
- $host="localhost";
- $username="root";
- $password="";
- $databasename="reservation";
+
+
+"Server=MYSQL5037.site4now.net;Database=db_a7cc25_reserva;Uid=a7cc25_reserva;Pwd=YOUR_DB_PASSWORD"
+ $host="MYSQL5037.site4now.net";
+ $username="a7cc25_reserva";
+ $password="mt00268018";
+ $databasename="db_a7cc25_reserva";
  $errors = array(); 
  $conn = new mysqli($host, $username, $password, $databasename);
 // Check connection
