e827c799635ef888e232542240ab199e62416ca3
bob-aila <61519243+bob-aila@users.noreply.github.com>
2021-11-28 17:30:44 +0000

    Add files via upload

---
+++ booking/terms_condition.php
@@ -0,0 +1,50 @@
+   <!--Modal Log-out --> 
+
+            <div class="modal fade" id="logout" tabindex="-1">
+            <div class="modal-dialog">
+              <div class="modal-content">
+                <div class="modal-header">
+                </div>
+                <div class="modal-body">
+
+<body>
+<div class="view">
+  <p>
+Booking Terms &amp; Conditions
+<ol>
+<p>We are eagerly anticipating your arrival and would like to advise you of the following in order to help you with your trip planning.Your reservation number is <b><?php echo $_SESSION['confirmation']?>:</b><br/><br/>Should there be a concern with your reservation, a customer service representative will contact you. Otherwise, consider your reservation confirmed.</p>
+<ul>
+ <li>Function Room rate is Ksh 500.00 for first four hours and Ksh 100.00 for each succeeding hours.</li>
+ <li>Outside foods and drinks<strong> are allowed </strong> inside the guest house.</li>
+ <li>Check in time is 1pm and Check out time is 10 am.</li>
+ <li>Guest arriving before 1 pm shall be accommodated if rooms are vacant and ready.</li>
+ <li>Free WIFI access.</li>
+ <li>Room rates inclusive of government tax and service charge.</li>
+ <li>Rates are subject to change without prior notice.</li>
+ <li>Cancellation notification must be made at least a day prior to arrival for refund of deposits. Cancellation received within the 10 days period will result to forfeiture of full deposits.</li>
+ <li>We organize Breakfast, Lunch and Dinner with our cafeteria upon request with 2 hours notice.</li><br>
+<strong>I have agreed that I will present the following documents upon check in:</strong><br/>
+ <li>Proof of M-Pesa or Bank Payment.</li>
+ <li>Authorization message or letter from payer for guest/s whose transactions were paid for in his/ her behalf.</li>
+ </ul>
+If you have any questions to, please email at gaila8218@gmail.com or call +254 780410173 we can arrange for a pic-up if requested
+<br/><br/>
+Thank you for choosing JIJO airBnbs Kenya.
+<br/><br/>
+Respectfully your,<br/><br/>
+Best BNB Patner Kenya.
+</p>
+</div>
+</body>
+</html>
+
+                </div>
+                <div class="modal-footer">
+                    <button class="btn" data-dismiss="modal" aria-hidden="true"><i class="icon-remove"></i> Close</button>
+                    
+                </div>
+              </div> 
+              </div>
+          </div>      
+                                      
+          <!--Logout end -->  
