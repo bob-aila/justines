aba92f3b4dabde4f043c51ef405f180cc4657a25
bob-aila <61519243+bob-aila@users.noreply.github.com>
2021-12-07 22:52:46 +0300

    Update index.php

--- index.php
+++ index.php
@@ -49,7 +49,7 @@ $(document).ready(function(){
 	            <span class="icon-bar"></span>
 	            <span class="icon-bar"></span>
 	          </button>
-	          <a class="navbar-brand" href="index.php"><img src="img/logo2.png" height="7%" width="25%"></a>
+	          <a class="navbar-brand" href="index.php"><img src="img/logo2.png" height="10%" width="40%"></a>
 	        </div>
 	        <div class="collapse navbar-collapse">
 	          <ul class="nav navbar-nav">
